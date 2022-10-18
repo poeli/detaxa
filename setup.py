f6386756bf08a6fcbfad26d05ced93b557797899
poeli <po-e@lanl.gov>
2022-10-18 14:13:22 -0600

    add descriptions

--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
 from setuptools import setup, find_packages
 
-from src.pytaxa import __version__ as ptv
+from src.detaxa import __version__ as ptv
 
 INSTALL_REQUIRES = [
     pkg for pkg in open('requirements.txt').readlines()
 ]
 
 setup(
-    name='pytaxa',
+    name='detaxa',
     version= ptv,
     author='Paul Li',
     author_email='po-e@lanl.gov',
     url='https://github.com/poeli/taxonomy',
     license='LICENSE',
-    packages=['pytaxa'],
-    # packages=find_packages(include=['pytaxa', 'pytaxa.*']),
+    packages=['detaxa'],
+    # packages=find_packages(include=['detaxa', 'detaxa.*']),
     include_package_data=True,
     package_dir={'': 'src'},
-    package_data={'pytaxa':['taxonomy_db/*.json']},
+    package_data={'detaxa':['taxonomy_db/*.json']},
     description='NCBI taxonomy and lineage lookup',
     keywords=["taxonomy"],
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     entry_points={ 
-        'console_scripts': ['pytaxa = pytaxa.__main__:cli' ] 
+        'console_scripts': ['detaxa = detaxa.__main__:cli' ] 
     },
     install_requires=INSTALL_REQUIRES,
     classifiers=[
